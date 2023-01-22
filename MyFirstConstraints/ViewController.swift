5b5cecc37982d0d9fd9644773192e9117da1ac20
Andrey <amachulin@mac.com>
2023-01-22 20:42:42 +0100

    Initial Commit

---
+++ MyFirstConstraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstConstraints
+//
+//  Created by Andrey Machulin on 22.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
